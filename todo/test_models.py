2ec935266013eba88a1c6d409594ee9fe5691fa3
RQISL <creativewb50@gmail.com>
2023-06-24 17:41:46 +0000

    Walkthrough 4

---
+++ todo/test_models.py
@@ -0,0 +1,15 @@
+# Test for Models
+
+from django.test import TestCase
+from .models import Item
+
+
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Tode Item')
+        self.assertFalse(item.done)
+
+    def test_item_string_methond_return_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
