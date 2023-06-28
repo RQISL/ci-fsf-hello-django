546cbb577bc1e760b2bfaa852f2e2a34ab024dae
RQISL <creativewb50@gmail.com>
2023-06-28 18:30:05 +0000

    Re-fix bugs in settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +29,7 @@ SECRET_KEY = 'django-insecure-ivv^a1l4jom3x2gnh0pr(*%j*$-8_b!iq07gkee$hh',
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rqisl-ci-fsf-hello-djang-6f49yqvs9k.us2.codeanyapp.com',
-                 'rq3456-djando-todo-app-075ab1e8106e.herokuapp.com']
+                 os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -76,13 +78,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
